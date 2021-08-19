dacda9ef7733fb1fe3244f4e709c2d88f020107d
pullso <pullso.code@gmail.com>
2021-08-19 13:26:13 +0300

    add tags from db

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from './tag.entity';
 
 @Module({
   controllers: [TagController],
   providers: [TagService],
+  imports: [TypeOrmModule.forFeature([TagEntity])],
 })
 export class TagModule {}
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { TagEntity } from './tag.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['dragons'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
   }
 }
