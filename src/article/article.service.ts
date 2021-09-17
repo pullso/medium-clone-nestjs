39e1e9fd91ae647f5467d8eb736c8f80b27dd72c
pullso <pullso.code@gmail.com>
2021-09-17 11:51:11 +0300

    add dislikes for articles

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -139,4 +139,26 @@ export class ArticleService {
 
     return article;
   }
+
+  async deleteArticleFromFavorites(
+    slug: string,
+    currentUserId: number,
+  ): Promise<ArticleEntity> {
+    const article = await this.findBySlug(slug);
+    const user = await this.userRepository.findOne(currentUserId, {
+      relations: ['favorites'],
+    });
+    const articleIndex = user.favorites.findIndex(
+      (articleInFavorites) => articleInFavorites.id === article.id,
+    );
+
+    if (articleIndex >= 0) {
+      user.favorites.splice(articleIndex, 1);
+      article.favoritesCount--;
+      await this.userRepository.save(user);
+      await this.articleRepository.save(article);
+    }
+
+    return article;
+  }
 }
